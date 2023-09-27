e02ab63961f67ec7d3aad7231636113c28244891
amsterdam <amsterdam@amsterdam.amsterdam>
2023-09-27 00:45:13 -0500

    actualizacion CRM_OSSOLUCIONES-BACKEND: adicion de proyecto crm_ossoluciones de spring boot, creacion de carpeta de disenoDB

---
+++ Backend/crm_ossoluciones/src/main/java/com/amsterdam/crm_ossoluciones/CrmOssolucionesApplication.java
@@ -0,0 +1,13 @@
+package com.amsterdam.crm_ossoluciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmOssolucionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrmOssolucionesApplication.class, args);
+	}
+
+}
---
+++ Backend/crm_ossoluciones/src/test/java/com/amsterdam/crm_ossoluciones/CrmOssolucionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amsterdam.crm_ossoluciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmOssolucionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
